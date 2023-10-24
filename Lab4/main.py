a44f5f798faee83fb6a5b6a08ddd222ce918b63a
MistreanuEmanuela <100144278+MistreanuEmanuela@users.noreply.github.com>
2023-10-24 19:09:06 +0300

    complet

--- Lab4/main.py
+++ Lab4/main.py
@@ -38,6 +38,7 @@ alpha_max = 0
 alpha_values = np.linspace(10, 0, 1000)
 for alpha in alpha_values:
     timp_servire_95 = timp_servire_sub_15_minute(alpha, numar_simulari, lambda_v, plasare_comanda, deviatie_comanda)
+    # print(timp_servire_95)
     if timp_servire_95 <= 15:
         alpha_max = alpha
         break
@@ -46,3 +47,28 @@ if alpha_max > 0:
     print("Valoarea maximă a lui α pentru care timpul total de servire este sub 15 minute pentru 95% dintre clienți este:", alpha_max)
 else:
     print("Nu s-a găsit o valoare a lui α care să îndeplinească condiția.")
+
+
+#3..
+def average_waiting_time(alpha, num_simulations, plasare_comanda, deviatie_comanda):
+    waiting_times = []
+
+    for _ in range(num_simulations):
+        timp_comanda = norm.rvs(loc=plasare_comanda, scale=deviatie_comanda, size=numar_simulari)
+        timp_pregatire = expon.rvs(scale=alpha, size=numar_simulari)
+        timp_total = timp_comanda + timp_pregatire
+        waiting_time = timp_total
+        waiting_times.append(waiting_time)
+
+    average_wait_time = np.mean(waiting_times)
+    return average_wait_time
+
+
+average_wait_time = average_waiting_time(alpha_max, numar_simulari, plasare_comanda, deviatie_comanda)
+print(f"Timpul mediu de așteptare pentru a fi servit al unui client cu alpha={alpha_max} este {average_wait_time:.2f} minute.")
+
+
+
+
+
+
